39b86fdc77a39f04fcf5bbf97f4d05ad698566a8
Gabriel_Antezana <114163620+Gwiefubweuivwbvig89393475982734@users.noreply.github.com>
2026-01-20 15:09:25 +0000

    feat: add Button component and update Home page content

--- app/page.js
+++ app/page.js
@@ -1,4 +1,9 @@
 import Image from "next/image";
+function Button({text, color})
+{
+  return (<button className={`mr-8 h-12 w-full rounded-full bg-red-500 px-5 text-white transition-colors md:w-[158px]`}>{text}</button>)
+  
+}
 
 export default function Home() {
   return (
@@ -11,10 +16,12 @@ export default function Home() {
           width={100}
           height={20}
           priority
-        />
+          
+        /> 
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
+            <Button text="Hello" color="red-500"></Button>
+            To review platforms view below.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
